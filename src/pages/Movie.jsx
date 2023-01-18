b093cb6b24d1ad685c17d4eda8813eddf837fea9
Maria Tati <mauramaria92@gmail.com>
2023-01-18 23:33:26 +0000

    removed loader

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -49,7 +49,6 @@ const Movies = () => {
       <div className='max-w-5xl mx-auto p-4'>
         <h2 className='font-bold text-4xl text-center pb-10'>Movie Details</h2>
         <article className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-          {loading && <Loader />}
           {movie &&
            <>
             <div className=''>
