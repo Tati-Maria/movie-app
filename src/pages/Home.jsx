42e9e2a51c26ed09dc396dd7832701b220c2c19a
Maria Tati <mauramaria92@gmail.com>
2023-01-18 22:33:50 +0000

    fixed header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <section className="py-12 font-nunito">
     <div className="max-w-5xl mx-auto p-4">
-        <h2 className="text-center text-2xl font-bold pb-10">The Most Popular Movies in 2022:</h2>
+        <h2 className="text-center text-2xl font-bold pb-10">The Most Popular Movies</h2>
         <section className="grid grid-cols-1 place-items-center gap-10 sm:grid-cols-2 lg:grid-cols-3">
             {error && <span>{error.message}</span>}
             {loading && <Loader />}
